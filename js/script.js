584311035aa8406d1fbe491215bc08931f555227
juanfug <156391637+Jutria23@users.noreply.github.com>
2024-01-24 11:41:08 -0500

    scroll
    json para llamado de imagenes para srcoll

--- js/script.js
+++ js/script.js
@@ -406,3 +406,47 @@ Pension.findById(pensionId, (err, pension) => {
   // Cierra la conexión a la base de datos después de realizar la operación
   mongoose.connection.close();
 });
+
+// Scroll Infinito
+
+let page = 1; // Número de página inicial
+const perPage = 2; // Número de imágenes por página
+
+// Función para cargar imágenes desde el archivo JSON local
+function loadImages(img, ) {
+    $.ajax({
+        url: 'imagenes.json',
+        method: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            const startIndex = (page - 1) * perPage;
+            const endIndex = startIndex + perPage;
+            const images = data.slice(startIndex, endIndex);
+            displayImages(images); // Función para mostrar imágenes en la página
+            page++; // Incrementa el número de página para la próxima carga
+        },
+        error: function (error) {
+            console.error('Error al cargar imágenes:', error);
+        }
+    });
+}
+
+// Función para mostrar imágenes en la página
+function displayImages(images) {
+    const galeria = $('#galeria');
+    
+    images.forEach(image => {
+        const imgElement = `<img src="${image.url}" alt="${image.alt}">`;
+        galeria.append(imgElement);
+    });
+}
+
+// Función para verificar si el usuario ha llegado al final de la página
+$(window).scroll(function () {
+    if ($(window).scrollTop() + $(window).height() >= $(document).height() - 100) {
+        loadImages(); // Carga más imágenes cuando el usuario llega al final
+    }
+});
+
+// Carga las primeras imágenes al cargar la página
+loadImages();
